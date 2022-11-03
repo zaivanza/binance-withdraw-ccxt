4ce8aceb9a475beda61854a0feaf9775944b33a7
hodlmod.eth <93076644+zaivanza@users.noreply.github.com>
2022-11-03 15:17:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 
 def check_balance(address, number, web3):
     try:
-        balance = web3.eth.get_balance(address)
+        balance = web3.eth.get_balance(web3.toChecksumAddress(address))
         humanReadable = web3.fromWei(balance,'ether')
         cprint(f'{number}. {address} : {humanReadable}', 'white')
 
