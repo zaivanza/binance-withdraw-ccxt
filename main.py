50b7cc620aceeb8c67a9625f2a7c6dce547cea06
hodlmod.eth <93076644+zaivanza@users.noreply.github.com>
2023-01-18 07:58:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ from termcolor import cprint
 from web3 import Web3
 import random
 
-def check_balance(address, number, web3):
-    try:
-        balance = web3.eth.get_balance(web3.toChecksumAddress(address))
-        humanReadable = web3.fromWei(balance,'ether')
-        cprint(f'{number}. {address} : {humanReadable}', 'white')
-
-    except Exception as error:
-        cprint(f'{number}. {address} = {error}', 'red')
 
 def binance_withdraw(address, amount_to_withdrawal, symbolWithdraw, network, API_KEY, API_SECRET):
 
@@ -46,22 +38,9 @@ if __name__ == "__main__":
     symbolWithdraw = 'ETH'
     network = 'Arbitrum'
 
-    # RPC = 'https://mainnet.optimism.io'
-    # RPC = 'https://bsc-dataseed.binance.org'
-    # RPC = 'https://polygon-rpc.com'
-    RPC = 'https://arb1.arbitrum.io/rpc'
-    web3 = Web3(Web3.HTTPProvider(RPC))
-
     # api_keys of binance
     API_KEY = "your_api_key"
     API_SECRET = "your_api_secret"
-
-    cprint('\a\n/// start check balance...', 'white')
-    number = 0
-    for wallet in wallets_list:
-        address = web3.toChecksumAddress(wallet)
-        number = number + 1
-        check_balance(address, number, web3) # проверяет баланс ETH
 
     cprint('\a\n/// start withdrawing...', 'white')
     for wallet in wallets_list:
